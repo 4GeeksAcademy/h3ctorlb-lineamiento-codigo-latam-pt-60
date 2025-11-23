0fa598a99f553499f93172e5c97182383e9aaeb2
Hector Legorreta Becerril <hola@hectorlb.com>
2025-11-23 23:34:26 +0000

    funciones

--- src/app.js
+++ src/app.js
@@ -17,3 +17,45 @@ function suma(numeros) {
   
   const numbers = [1, -2, 3, -4, 5, -6, 7, -8, 9, -10];
   console.log(suma(numbers));
+
+  function isOldEnoughToDrive(age) {
+  if (age >= 16) return "Yes"
+  return "No";
+}
+console.log(isOldEnoughToDrive(18)); // Output: "Yes"
+
+/* function numberIsDivisible(num) {
+  if (num > 0) {
+    if (num % 2 === 0) {
+      if (num % 3 === 0) {
+        return "Divisible by 2 and 3";
+      } else {
+        return "Divisible by 2";
+      }
+    } else {
+      if (num % 3 === 0) {
+        return "Divisible by 3";
+      } else {
+        return "Not divisible by 2 or 3";
+      }
+    }
+  } else {
+    return "Number is not positive";
+  } */
+
+
+  function numberIsDivisible(num) {
+  if (num <= 0) {
+    return "Number is not positive";
+  }
+  if (num % 2 === 0 && num % 3 === 0) {
+    return "Divisible by 2 and 3";
+  }
+  if (num % 2 === 0) {
+    return "Divisible by 2";
+  }
+  if (num % 3 === 0) {
+    return "Divisible by 3";
+  }
+  return "Not divisible by 2 or 3";
+}
