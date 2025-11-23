a57e2c46472445aa85cb4828eaa15d3c5dc1139b
Hector Legorreta Becerril <hola@hectorlb.com>
2025-11-23 23:24:34 +0000

    correcion funcion

--- src/app.js
+++ src/app.js
@@ -5,7 +5,15 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function suma(numeros) {
+    let total = 0;
+    for (let i = 0; i < numeros.length; i++) {
+      if (numeros[i] > 0) {
+        total += numeros[i];
+      }
+    }
+    return total;
+  }
+  
+  const numbers = [1, -2, 3, -4, 5, -6, 7, -8, 9, -10];
+  console.log(suma(numbers));
